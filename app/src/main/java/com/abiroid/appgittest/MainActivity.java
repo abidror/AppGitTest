c7d647f6cab6f8b1fc2f183a381c44a795cbd884
abidror <abid93@gmail.com>
2015-06-15 18:13:45 +0500

    To Do List

--- app/src/main/java/com/abiroid/appgittest/MainActivity.java
+++ app/src/main/java/com/abiroid/appgittest/MainActivity.java
@@ -1,17 +1,70 @@
 package com.abiroid.appgittest;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    EditText edItem;
+    Button btnAddItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvItems = (ListView)findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+
+        items.add("Item 1");
+        items.add("Item 2");
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+
+        edItem = (EditText)findViewById(R.id.edItem);
+        btnAddItem = (Button)findViewById(R.id.btnAddItem);
+
+        btnAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String newItem = edItem.getText().toString();
+
+                itemsAdapter.add(newItem);
+
+                edItem.setText("");
+
+            }
+        });
+
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+
+                return false;
+            }
+        });
+
     }
 
     @Override
